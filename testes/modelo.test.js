e65a418ea1368d12f285f1339a499fbd3d2cf620
Filipe <103159224+Arflipe@users.noreply.github.com>
2024-12-11 16:16:57 -0300

    Teste de integração

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -23,3 +23,29 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
 });
+
+test('Testando cadastro de três respostas', () => {
+  const idPergunta = modelo.cadastrar_pergunta('1 + 1 = ?');
+
+  modelo.cadastrar_resposta(idPergunta, '1');
+  modelo.cadastrar_resposta(idPergunta, '2');
+  modelo.cadastrar_resposta(idPergunta, '3');
+
+  const respostas = modelo.get_respostas(idPergunta);
+
+  expect(respostas.length).toBe(3);
+
+  expect(respostas[0].texto).toBe('1');
+  expect(respostas[1].texto).toBe('2');
+  expect(respostas[2].texto).toBe('3');
+});
+
+test('Testando pegar a pergunta', () => {
+  const idPergunta = modelo.cadastrar_pergunta('1 + 1 = ?');
+  const pergunta = modelo.get_pergunta(idPergunta);
+  expect(pergunta).toEqual({
+    id_pergunta: idPergunta,
+    texto: '1 + 1 = ?',
+    id_usuario: 1
+  });
+});
